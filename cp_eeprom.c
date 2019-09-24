07247fce1a2688113041c7440c6145c629f03d07
kozova1 <mug66kk@gmail.com>
2019-09-24 20:50:21 +0300

    fixed name, paging is not mount-and-forget but constant update unfortunately :(

---
+++ cp_eeprom.c
@@ -0,0 +1,9 @@
+#include "cpu.h"
+#include "includes.h"
+#include "set.h"
+
+void cp_eeprom(void) {
+	// experimental
+	memcpy(MMAP.EEPROM_WINDOW, (FLASH_EEPROM + EEPROM_PAGE * 0x3FFF),
+		   sizeof(uint8_t) * 0x3FFF);
+}
--- mount_eeprom.c
+++
@@ -1,8 +0,0 @@
-#include "cpu.h"
-#include "includes.h"
-#include "set.h"
-
-void mount_eeprom(void) {
-	// experimental
-	memcpy(MMAP.EEPROM_WINDOW, (FLASH_EEPROM + EEPROM_PAGE * 0x3FFF), sizeof(uint8_t) * 0x3FFF);
-}
