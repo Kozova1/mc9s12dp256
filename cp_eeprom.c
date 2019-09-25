38a2ee0495671d5eba7f5e04d83c7b6c59d93793
Kozova1 <mug66kk@gmail.com>
2019-09-25 08:33:08 +0300

    cp_eeprom.c is better now

--- cp_eeprom.c
+++ cp_eeprom.c
@@ -2,8 +2,26 @@
 #include "includes.h"
 #include "set.h"
 
-void cp_eeprom(void) {
+int eeprom_to_memory(void) {
 	// experimental
-	memcpy(MMAP.EEPROM_WINDOW, (FLASH_EEPROM + EEPROM_PAGE * 0x3FFF),
-		   sizeof(uint8_t) * 0x3FFF);
+	int *a = (int*)memcpy(MMAP.EEPROM_WINDOW,
+		   				  (FLASH_EEPROM + EEPROM_PAGE * 0x3FFF),
+		   				  sizeof(uint8_t) * 0x3FFF);
+	if (a == NULL) {
+		return -1;
+	} else {
+		return 0;
+	}
+}
+
+int memory_to_eeprom(void) {
+	// experimental
+	int *a = (int*)memcpy((FLASH_EEPROM + EEPROM_PAGE * 0x3FFF),
+						  MMAP.EEPROM_WINDOW,
+						  sizeof(uint8_t) * 0x3FFF);
+	if (a == NULL) {
+		return -1;
+	} else {
+		return 0;
+	}
 }
